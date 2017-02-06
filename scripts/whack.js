e5231a5b7b7170971e2067aec626a2184b4d5cb5
Mel Huang <hello@melaniehuang.com>
2017-02-07 00:34:36 +1100

    getting keyboard to work

--- scripts/whack.js
+++ scripts/whack.js
@@ -1,28 +1,53 @@
-var init = function () {
-};
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload() {
-};
+
+    game.load.image('phaser', 'assets/sprites/character.png');
+
+}
+
+var sprite;
+
+var upKey;
+var downKey;
+var leftKey;
+var rightKey;
 
 function create() {
-};
 
-window.onload = function() { 
-  var wfconfig = {
-    active: function() { 
-      console.log("font loaded");
-      init();
-    },
-    
-    inactive: function() {
-        console.log("fonts could not be loaded!");
-        init();
-    },
+    game.stage.backgroundColor = '#736357';
 
-    google: {
-      families: ["Press Start 2P"]
+    sprite = game.add.sprite(300, 300, 'phaser');
+
+    upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+
+}
+
+function update() {
+
+    if (upKey.isDown)
+    {
+      game.stage.backgroundColor = "#30d4f0";
+      sprite.y--;
     }
-  };
+    else if (downKey.isDown)
+    {
+        game.stage.backgroundColor = "#a9ffe3";
+        sprite.y++;
+    }
 
-  WebFont.load(wfconfig);
-};
+    if (leftKey.isDown)
+    {
+        game.stage.backgroundColor = "#f8f118";
+        sprite.x--;
+    }
+    else if (rightKey.isDown)
+    {
+        game.stage.backgroundColor = "#ef828f";
+        sprite.x++;
+    }
+
+}
